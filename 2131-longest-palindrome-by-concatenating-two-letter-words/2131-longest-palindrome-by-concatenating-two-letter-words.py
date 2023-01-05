3ba9a6c27ae6e43697de7acf85e9e0f0f0117f28
Prashant Kumar <72797072+mrprashantkumar@users.noreply.github.com>
2023-01-05 23:06:53 +0530

    Time: 2217 ms (56.19%), Space: 38.4 MB (24.93%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.py
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.py
@@ -3,16 +3,16 @@ class Solution:
         d = Counter(words)
         
         flag = True
-        same, diff = 0, 0
+        ans = 0
         for word in d:
             if word[0] == word[1]:
                 if d[word]%2==0:
-                    same += d[word]
+                    ans += d[word]
                 else:
-                    same += (d[word]-1)
+                    ans += (d[word]-1)
                     if flag:
-                        same += 1
+                        ans += 1
                         flag = False
             else:
-                diff += min(d[word], d[word[::-1]])
-        return (same+diff)*2
+                ans += min(d[word], d[word[::-1]])
+        return ans*2
