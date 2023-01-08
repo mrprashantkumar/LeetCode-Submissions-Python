ba7ac1c6407b62ecff68f0c13c83a648c4421602
Prashant Kumar <72797072+mrprashantkumar@users.noreply.github.com>
2023-01-08 21:26:10 +0530

    Time: 80 ms (91.09%), Space: 13.8 MB (94.26%) - LeetHub

---
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.py
@@ -0,0 +1,23 @@
+class Solution:
+    def maxPoints(self, points: List[List[int]]) -> int:
+        n = len(points)
+        if n<=2:
+            return n
+        
+        def getSlope(p1, p2):
+            x1, y1 = p1
+            x2, y2 = p2
+            if x1==x2:
+                return 1000000007
+            return (y2-y1)/(x2-x1)
+            
+        ans = 1
+        for i in range(n):
+            d = defaultdict(int)
+            for j in range(i+1, n):
+                if i != j:
+                    slope = getSlope(points[i], points[j])
+                    d[slope] += 1
+                    ans = max(d[slope], ans)
+        return ans+1
+                    
