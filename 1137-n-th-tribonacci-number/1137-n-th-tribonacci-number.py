271b16caa64544568d9fd3c96f48be95f8cf2b20
Prashant Kumar <72797072+mrprashantkumar@users.noreply.github.com>
2023-01-30 11:25:45 +0530

    Time: 37 ms (44.79%), Space: 13.8 MB (95.87%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
@@ -6,7 +6,7 @@ class Solution:
             return 1
         
         a, b, c = 0, 1, 1
-        for i in range(n):
+        for i in range(n-2):
             a, b, c = b, c, a+b+c
         
-        return a
+        return c
