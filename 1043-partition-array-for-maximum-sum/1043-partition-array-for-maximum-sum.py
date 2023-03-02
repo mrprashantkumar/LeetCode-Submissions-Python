dd0647e57f5f0737c6420f46aeb348514619d280
Prashant Kumar <72797072+mrprashantkumar@users.noreply.github.com>
2023-03-02 21:42:01 +0530

    Time: 594 ms (61.07%), Space: 15.2 MB (20.92%) - LeetHub

---
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxSumAfterPartitioning(self, arr: List[int], k: int) -> int:
+        def helper(i):
+            if i==n:
+                return 0
+            
+            if dp[i] != -1:
+                return dp[i]
+            ans = 0
+            maxsofar = arr[i]
+            for j in range(i, min(n, i+k)):
+                maxsofar = max(maxsofar, arr[j])
+                smans = (j-i+1)*maxsofar + helper(j+1)
+                ans = max(ans, smans)
+            
+            dp[i] = ans
+            return ans
+        
+        n = len(arr)
+        dp = [-1]*n
+        return helper(0)
