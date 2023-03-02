887c9d583832b5c752b238ae1f7057472588f8ae
Prashant Kumar <72797072+mrprashantkumar@users.noreply.github.com>
2023-03-02 21:44:56 +0530

    Time: 472 ms (96.11%), Space: 13.8 MB (93.19%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.py
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.py
@@ -1,21 +1,36 @@
 class Solution:
     def maxSumAfterPartitioning(self, arr: List[int], k: int) -> int:
-        def helper(i):
-            if i==n:
-                return 0
+#         def helper(i):
+#             if i==n:
+#                 return 0
             
-            if dp[i] != -1:
-                return dp[i]
+#             if dp[i] != -1:
+#                 return dp[i]
+#             ans = 0
+#             maxsofar = arr[i]
+#             for j in range(i, min(n, i+k)):
+#                 maxsofar = max(maxsofar, arr[j])
+#                 smans = (j-i+1)*maxsofar + helper(j+1)
+#                 ans = max(ans, smans)
+            
+#             dp[i] = ans
+#             return ans
+        
+#         n = len(arr)
+#         dp = [-1]*n
+#         return helper(0)
+        
+        n = len(arr)
+        dp = [-1]*(n+1)
+        
+        for i in range(n-1, -1, -1):
             ans = 0
             maxsofar = arr[i]
             for j in range(i, min(n, i+k)):
                 maxsofar = max(maxsofar, arr[j])
-                smans = (j-i+1)*maxsofar + helper(j+1)
+                smans = (j-i+1)*maxsofar + dp[j+1]
                 ans = max(ans, smans)
             
             dp[i] = ans
-            return ans
         
-        n = len(arr)
-        dp = [-1]*n
-        return helper(0)
+        return dp[0]+1
