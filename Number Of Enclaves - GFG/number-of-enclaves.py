3d681036d7b29a884994e462818233443760a22b
Prashant Kumar <72797072+mrprashantkumar@users.noreply.github.com>
2023-03-28 15:36:59 +0530

    Added solution - LeetHub

---
+++ Number Of Enclaves - GFG/number-of-enclaves.py
@@ -0,0 +1,54 @@
+#User function Template for python3
+from typing import List
+import sys
+sys.setrecursionlimit(10**7)
+class Solution:    
+    def numberOfEnclaves(self, grid: List[List[int]]) -> int:
+        # code here
+        def isvalid(p, q):
+            return 0<=p<n and 0<=q<m and grid[p][q] == 1 and (p, q) not in visited
+        
+        def dfs(x, y):
+            visited.add((x, y))
+            for dx, dy in [(-1, 0), (0, -1), (0, 1), (1, 0)]:
+                if isvalid(x+dx, y+dy):
+                    dfs(x+dx, y+dy)
+        
+        n, m = len(grid), len(grid[0])
+        visited = set()
+        for i in range(n):
+            if grid[i][0] == 1 and (i, 0) not in visited:
+                visited.add((i, 0))
+                dfs(i, 0)
+            if grid[i][m-1] == 1 and (i, m-1) not in visited:
+                visited.add((i, m-1))
+                dfs(i, m-1)
+        for j in range(m):
+            if grid[0][j] == 1 and (0, j) not in visited:
+                visited.add((0, j))
+                dfs(0, j)
+            if grid[n-1][j] == 1 and (n-1, j) not in visited:
+                visited.add((n-1, j))
+                dfs(n-1, j)
+        
+        ans = 0
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j] == 1 and (i, j) not in visited:
+                    ans += 1
+        return ans
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+if __name__ == "__main__":
+    for _ in range(int(input())):
+        n, m = map(int,input().strip().split())
+        grid = []
+        for i in range(n):
+            grid.append([int(i) for i in input().strip().split()])
+
+        obj=Solution()
+        print(obj.numberOfEnclaves(grid))
+# } Driver Code Ends
