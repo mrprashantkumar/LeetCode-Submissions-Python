a2dea5c2f2bf7b63fc3e784b7a87ccd8039fb954
Prashant Kumar <72797072+mrprashantkumar@users.noreply.github.com>
2023-03-29 12:05:19 +0530

    Time: 774 ms (16.77%), Space: 19.7 MB (21.84%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.py
+++ 1402-reducing-dishes/1402-reducing-dishes.py
@@ -1,19 +1,26 @@
 class Solution:
     def maxSatisfaction(self, satisfaction: List[int]) -> int:
-        def helper(i, time):
-            if i==n:
-                return 0
+#         def helper(i, time):
+#             if i==n:
+#                 return 0
             
-            if dp[i][time] != -1:
-                return dp[i][time]
+#             if dp[i][time] != -1:
+#                 return dp[i][time]
             
-            notTake = helper(i+1, time)
-            take = satisfaction[i]*time + helper(i+1, time+1)
+#             notTake = helper(i+1, time)
+#             take = satisfaction[i]*time + helper(i+1, time+1)
             
-            dp[i][time] = max(take, notTake)
-            return max(take, notTake)
+#             dp[i][time] = max(take, notTake)
+#             return max(take, notTake)
         
         satisfaction.sort()
         n = len(satisfaction)
-        dp = [[-1]*(n+1) for _ in range(n+1)]
-        return helper(0, 1)
+        dp = [[0]*(n+2) for _ in range(n+1)]
+        
+        for i in range(n-1, -1, -1):
+            for time in range(n, 0, -1):
+                notTake = dp[i+1][time]
+                take = satisfaction[i]*time + dp[i+1][time+1]
+                dp[i][time] = max(take, notTake)
+        
+        return dp[0][1]
