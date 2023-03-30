5ffdb98b4217ed42347db57b8da8fde808f2ee67
Prashant Kumar <72797072+mrprashantkumar@users.noreply.github.com>
2023-03-30 22:14:25 +0530

    Added solution - LeetHub

---
+++ Alien Dictionary - GFG/alien-dictionary.py
@@ -0,0 +1,81 @@
+#User function Template for python3
+from collections import *
+class Solution:
+    def findOrder(self,dict, N, K):
+        graph = defaultdict(list)
+        for i in range(n-1):
+            l = min(len(dict[i]), len(dict[i+1]))
+            for j in range(l):
+                if dict[i][j] != dict[i+1][j]:
+                    graph[dict[i][j]].append(dict[i+1][j])
+                    break
+        
+        indeg = [0]*K
+        for i in range(k):
+            for node in graph[chr(97+i)]:
+                indeg[ord(node)-97] += 1
+                
+        qu = deque()
+        for i in range(K):
+            if indeg[i] == 0:
+                qu.append(chr(97+i))
+        ans = []
+        while qu:
+            curr = qu.popleft()
+            ans.append(curr)
+            for nei in graph[curr]:
+                indeg[ord(nei)-97] -= 1
+                if indeg[ord(nei)-97] == 0:
+                    qu.append(nei)
+        
+        return ans
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+class sort_by_order:
+    def __init__(self,s):
+        self.priority = {}
+        for i in range(len(s)):
+            self.priority[s[i]] = i
+    
+    def transform(self,word):
+        new_word = ''
+        for c in word:
+            new_word += chr( ord('a') + self.priority[c] )
+        return new_word
+    
+    def sort_this_list(self,lst):
+        lst.sort(key = self.transform)
+
+if __name__ == '__main__':
+    t=int(input())
+    for _ in range(t):
+        line=input().strip().split()
+        n=int(line[0])
+        k=int(line[1])
+        
+        alien_dict = [x for x in input().strip().split()]
+        duplicate_dict = alien_dict.copy()
+        ob=Solution()
+        order = ob.findOrder(alien_dict,n,k)
+        s = ""
+        for i in range(k):
+            s += chr(97+i)
+        l = list(order)
+        l.sort()
+        l = "".join(l)
+        if s != l:
+            print(0)
+        else:
+            x = sort_by_order(order)
+            x.sort_this_list(duplicate_dict)
+            
+            if duplicate_dict == alien_dict:
+                print(1)
+            else:
+                print(0)
+
+
+# } Driver Code Ends
