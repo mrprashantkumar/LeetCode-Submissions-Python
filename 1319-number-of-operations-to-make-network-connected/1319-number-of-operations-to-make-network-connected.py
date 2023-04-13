05600ee12d41d8cbacc9347cd83f14306ab3e082
Prashant Kumar <72797072+mrprashantkumar@users.noreply.github.com>
2023-04-13 15:25:02 +0530

    Time: 497 ms (72.76%), Space: 49.1 MB (10.56%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.py
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.py
@@ -1,20 +1,25 @@
 class Solution:
     def makeConnected(self, n: int, connections: List[List[int]]) -> int:
-        if len(connections) < n - 1:
+        edges = len(connections)
+        if edges < n-1:
             return -1
         
-        graph = [set() for i in range(n)]
+        def dfs(node):
+            visited.add(node)
+            for nei in graph[node]:
+                if nei not in visited:
+                    dfs(nei)
+        
+        graph = defaultdict(list)
         for i, j in connections:
-            graph[i].add(j)
-            graph[j].add(i)
-        seen = [0] * n
-
-        def dfs(i):
-            if seen[i]:
-                return 0
-            seen[i] = 1
-            for j in graph[i]:
-                dfs(j)
-            return 1
-
-        return sum(dfs(i) for i in range(n)) - 1
+            graph[i].append(j)
+            graph[j].append(i)
+        
+        ans = 0
+        visited = set()
+        for i in range(n):
+            if i not in visited:
+                dfs(i)
+                ans += 1
+        
+        return ans-1
