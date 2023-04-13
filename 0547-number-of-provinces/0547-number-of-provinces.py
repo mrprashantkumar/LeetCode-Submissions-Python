e506e38c1d0690d89d9811a7b0dd4663ab87d7a4
Prashant Kumar <72797072+mrprashantkumar@users.noreply.github.com>
2023-04-13 15:06:19 +0530

    Time: 188 ms (68.20%), Space: 14.6 MB (31.95%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,18 +1,23 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        def helper(node):
-            for i, child in enumerate(isConnected[node]):
-                if child and i not in visited:
-                    visited.add(i)
-                    helper(i)
-            
-            
+        def dfs(node):
+            visited.add(node)
+            for nei in graph[node]:
+                if nei not in visited:
+                    dfs(nei)
+        
         n = len(isConnected)
+        graph = defaultdict(list)
+        for i in range(n):
+            for j in range(n):
+                if i != j and isConnected[i][j] == 1:
+                    graph[i].append(j)
+
         visited = set()
         ans = 0
         for i in range(n):
             if i not in visited:
-                visited.add(i)
-                helper(i)
+                dfs(i)
                 ans += 1
+        
         return ans
