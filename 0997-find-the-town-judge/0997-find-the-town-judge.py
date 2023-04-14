3ef42521a40b0da97e14c1832bee188e4c055fc0
Prashant Kumar <72797072+mrprashantkumar@users.noreply.github.com>
2023-04-15 00:14:21 +0530

    Time: 722 ms (89.87%), Space: 18.8 MB (99.76%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.py
+++ 0997-find-the-town-judge/0997-find-the-town-judge.py
@@ -1,12 +1,13 @@
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
-        graph = [[] for _ in range(n+1)]
+        indeg = [0]*(n+1)
+        outdeg = [0]*(n+1)
         
         for i, j in trust:
-            graph[i].append(j)
+            indeg[i] += 1
+            outdeg[j] += 1
         
-        # print(graph)
         for i in range(1,n+1):
-            if len(graph[i]) == 0 and all(i in graph[j] for j in range(1, n+1) if i != j):
+            if indeg[i] == 0 and outdeg[i]==n-1:
                 return i
         return -1
