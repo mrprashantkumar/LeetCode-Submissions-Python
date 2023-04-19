34aaa89fd1cb623e60562d47e2df9ad6aaf5d884
Prashant Kumar <72797072+mrprashantkumar@users.noreply.github.com>
2023-04-19 13:05:11 +0530

    Time: 23 ms (97.83%), Space: 13.8 MB (58.70%) - LeetHub

---
+++ 1104-path-in-zigzag-labelled-binary-tree/1104-path-in-zigzag-labelled-binary-tree.py
@@ -0,0 +1,18 @@
+class Solution:
+    def pathInZigZagTree(self, label: int) -> List[int]:
+        ans = []
+        temp = label
+        
+        level = 0
+        while temp:
+            temp //= 2
+            level += 1
+        
+        while level != 0:
+            ans.append(label)
+            maxi = pow(2, level) - 1
+            mini = pow(2, level -1) 
+            label = int((maxi+mini-label)/2)
+            level -= 1
+        
+        return ans[::-1]
