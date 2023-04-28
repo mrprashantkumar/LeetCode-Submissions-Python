8f8aa05c9535f0a34ade4e8b3dc53756fb62afcd
Prashant Kumar <72797072+mrprashantkumar@users.noreply.github.com>
2023-04-28 15:08:38 +0530

    Time: 1996 ms (25.24%), Space: 17.5 MB (10.16%) - LeetHub

---
+++ 0839-similar-string-groups/0839-similar-string-groups.py
@@ -0,0 +1,19 @@
+class Solution:
+    def numSimilarGroups(self, strs: List[str]) -> int:
+        p = {}
+        def find(x):
+            p.setdefault(x, x)
+            if x != p[x]:
+                p[x] = find(p[x])
+            return p[x]
+        
+        def union(x, y):
+            p[find(x)] = find(y)
+        
+        similar = lambda x, y: sum(a != b for a, b in zip(x, y)) <= 2
+        for i in range(len(strs)):
+            for j in range(i):
+                if similar(strs[i], strs[j]):
+                    union(strs[i], strs[j])
+                    
+        return len({find(x) for x in strs})
