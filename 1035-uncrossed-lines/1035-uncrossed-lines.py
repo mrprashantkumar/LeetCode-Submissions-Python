5b12180e453b4014fbc1794738e80c98484b3ecf
Prashant Kumar <72797072+mrprashantkumar@users.noreply.github.com>
2023-05-11 11:42:12 +0530

    Time: 329 ms (23.62%), Space: 18.2 MB (22.83%) - LeetHub

--- 1035-uncrossed-lines/1035-uncrossed-lines.py
+++ 1035-uncrossed-lines/1035-uncrossed-lines.py
@@ -8,11 +8,12 @@ class Solution:
                 return dp[i][j]
             
             if nums1[i] == nums2[j]:
-                dp[i][j] = 1 + helper(i+1, j+1)
-                return dp[i][j]
+                ans = 1 + helper(i+1, j+1)
             else:
-                dp[i][j] = max(helper(i+1, j), helper(i, j+1))
-                return dp[i][j]
+                ans = max(helper(i+1, j), helper(i, j+1))
+            
+            dp[i][j] = ans
+            return dp[i][j]
         
         n, m = len(nums1), len(nums2)
         dp = [[-1]*m for _ in range(n)]
