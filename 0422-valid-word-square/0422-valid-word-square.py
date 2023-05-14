46ad8e2b7bea76c68220f0becc63713740c72b35
Prashant Kumar <72797072+mrprashantkumar@users.noreply.github.com>
2023-05-15 00:33:47 +0530

    Time: 88 ms (27.81%), Space: 17.1 MB (13.90%) - LeetHub

--- 0422-valid-word-square/0422-valid-word-square.py
+++ 0422-valid-word-square/0422-valid-word-square.py
@@ -1,10 +1,6 @@
 class Solution:
     def validWordSquare(self, words: List[str]) -> bool:
         n= len(words)
-        for i in range(n-1):
-            if len(words[i]) < len(words[i+1]):
-                return False
-        
         for i in range(n):
             m = len(words[i])
             for j in range(m):
