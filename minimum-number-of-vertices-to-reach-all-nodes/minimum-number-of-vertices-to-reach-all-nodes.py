8a754c7ac7832f70c9705ea2012226622f3492bf
Prashant Kumar <72797072+mrprashantkumar@users.noreply.github.com>
2023-05-18 12:34:30 +0530

    Time: 1179 ms (55.98%) | Memory: 55.1 MB (16.24%) - LeetSync

--- minimum-number-of-vertices-to-reach-all-nodes/minimum-number-of-vertices-to-reach-all-nodes.py
+++ minimum-number-of-vertices-to-reach-all-nodes/minimum-number-of-vertices-to-reach-all-nodes.py
@@ -3,8 +3,5 @@ class Solution:
         indeg = [0]*n
         for i, j in edges:
             indeg[j] += 1
-        ans = []
-        for i in range(n):
-            if indeg[i] == 0:
-                ans.append(i)
-        return ans
+        
+        return [i for i in range(n) if indeg[i] == 0]
