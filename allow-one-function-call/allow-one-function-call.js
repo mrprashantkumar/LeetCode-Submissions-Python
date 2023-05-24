ea330c421c37b7ab22c1b28e4775cc44c399a53b
Prashant Kumar <72797072+mrprashantkumar@users.noreply.github.com>
2023-05-25 01:18:44 +0530

    Time: 55 ms (76.80%) | Memory: 42.2 MB (17.62%) - LeetSync

---
+++ allow-one-function-call/allow-one-function-call.js
@@ -0,0 +1,24 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function(fn) {
+    let flag = false;
+    return function(...args){
+        if (flag) {
+        return undefined;
+        } else {
+        flag = true;
+        return fn(...args);
+        }
+            
+    }
+};
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
