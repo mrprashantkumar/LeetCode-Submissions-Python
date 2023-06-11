64be9c958da2b085223e920272ae6483fcb46ea9
Prashant Kumar <72797072+mrprashantkumar@users.noreply.github.com>
2023-06-11 22:03:49 +0530

    Time: 328 ms (55.56%) | Memory: 23.9 MB (11.11%) - LeetSync

---
+++ lexicographically-smallest-string-after-substring-operation/lexicographically-smallest-string-after-substring-operation.py
@@ -0,0 +1,27 @@
+class Solution:
+    def smallestString(self, s: str) -> str:
+        arr = [i for i in s]
+        n = len(arr)
+        if all(i == 'a' for i in arr):
+            arr[-1] = 'z'
+            return "".join(arr)
+
+        i = 0
+        while i<n and arr[i] != 'a':
+            i += 1
+        
+        if i == 0:
+            while i<n and arr[i] == 'a':
+                i += 1
+            
+            j = i
+            while j<n and arr[j] != 'a':
+                j += 1
+            # print(i, j)
+            for k in range(i, j):
+                arr[k] = chr(ord(arr[k])-1)
+        else:
+            for k in range(i):
+                arr[k] = chr(ord(arr[k])-1)
+        
+        return "".join(arr)
