9223cd793f8a93005e0200711036a394bdb8515e
Prashant Kumar <72797072+mrprashantkumar@users.noreply.github.com>
2023-06-12 21:53:05 +0530

    Added solution - LeetHub

--- Strongly Connected Components (Kosaraju's Algo) - GFG/strongly-connected-components-kosarajus-algo.py
+++ Strongly Connected Components (Kosaraju's Algo) - GFG/strongly-connected-components-kosarajus-algo.py
@@ -1,32 +1,4 @@
 #User function Template for python3
-class DisjointSet:
-    def __init__(self, n):
-        self.rank = [0]*(n+1)
-        self.size = [1]*(n+1)
-        self.parent = [i for i in range(n+1)]
-    
-    def find(self, node):
-        if node == self.parent[node]:
-            return node
-        self.parent[node] = self.find(self.parent[node])
-        return self.parent[node]
-    
-    def union(self, u, v):
-        paru = self.find(u)
-        parv = self.find(v)
-        
-        if paru == parv:
-            return
-        
-        if self.rank[paru] < self.rank[parv]:
-            self.parent[paru] = parv
-        elif self.rank[paru] < self.rank[parv]:
-            self.parent[parv] = paru
-        else:
-            self.parent[parv] = paru
-            self.rank[paru] += 1
-    
-    
 class Solution:
     #Function to find number of strongly connected components in the graph.
     def kosaraju(self, V, adj):
