2c30120f4bf04c00fb7d91ac27d67efba70da352
Prashant Kumar <72797072+mrprashantkumar@users.noreply.github.com>
2023-07-17 23:27:42 +0530

    Time: 3095 ms (15.17%) | Memory: 0B (100.00%) - LeetSync

--- 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -1,8 +1,7 @@
 # Write your MySQL query statement below
 SELECT Q1.person_name
-FROM Queue AS Q1
-LEFT JOIN Queue AS Q2
-ON Q1.turn >= Q2.turn
+FROM Queue AS Q1, Queue AS Q2
+WHERE Q1.turn >= Q2.turn
 GROUP BY Q1.person_name
 HAVING SUM(Q2.weight) <= 1000
 ORDER BY SUM(Q2.weight) DESC LIMIT 1
