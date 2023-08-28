41903d2a770a238951d014e36be82ac49ba37e5d
Prashant Kumar <72797072+mrprashantkumar@users.noreply.github.com>
2023-08-28 17:48:51 +0530

    Time: 72 ms (82.86%) | Memory: 16.5 MB (31.21%) - LeetSync

---
+++ 1407-group-the-people-given-the-group-size-they-belong-to/group-the-people-given-the-group-size-they-belong-to.py
@@ -0,0 +1,15 @@
+class Solution:
+    def groupThePeople(self, groupSizes: List[int]) -> List[List[int]]:
+        graph = defaultdict(list)
+        for i, x in enumerate(groupSizes):
+            graph[x].append(i)
+        
+        ans = []
+        for size in graph:
+            l = len(graph[size])
+            if l == size:
+                ans.append(graph[size])
+            else:
+                for a in range(0, l, size):
+                    ans.append(graph[size][a:a+size])
+        return ans
