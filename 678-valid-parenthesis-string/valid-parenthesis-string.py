1d2e7c2ff6b61faca66e528f6f28a9b67f3c4d86
Prashant Kumar <72797072+mrprashantkumar@users.noreply.github.com>
2023-09-25 22:52:54 +0530

    Time: 51 ms (9.27%) | Memory: 17.3 MB (7.99%) - LeetSync

---
+++ 678-valid-parenthesis-string/valid-parenthesis-string.py
@@ -0,0 +1,19 @@
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        @cache
+        def helper(i, open):
+            if open < 0:
+                return False
+            if i == n:
+                return open == 0
+            
+            if s[i] == '(':
+                return helper(i+1, open+1)
+            elif s[i] == ')':
+                return helper(i+1, open-1)
+            else:
+                return helper(i+1, open) or helper(i+1, open+1) or helper(i+1, open-1)
+        
+
+        n = len(s)
+        return helper(0, 0)
