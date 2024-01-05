99060ebff325479b8a9a2e71d629466c29a458cf
Prashant Kumar <72797072+mrprashantkumar@users.noreply.github.com>
2024-01-05 13:07:07 +0530

    Time: 49 ms (99.97%) | Memory: 17.5 MB (28.11%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.py
@@ -1,15 +1,20 @@
 setrecursionlimit(10**8)
 class Solution:
-    def lengthOfLIS(self, arr: List[int]) -> int:
-        n = len(arr)
-        dp = [[0]*(n+1) for _ in range(n+1)]
-        for i in range(n-1, -1, -1):
-            for prev in range(i-1, -2, -1):
-                notTake = dp[i+1][prev+1]
-                take = 0
-                if prev == -1 or arr[i]>arr[prev]:
-                    take = 1+dp[i+1][i+1]
-
-                dp[i][prev+1] = max(take, notTake)
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        n = len(nums)
+        seen = []
+        for i in nums:
+            if not seen or seen[-1]<i:
+                seen.append(i)
+            else:
+                ind = bisect_left(seen, i)
+                seen[ind] = i
+        return len(seen)
         
-        return dp[0][0]
+        
+#         # dp = [1] * n
+#         # for i in range(n):
+#         #     for j in range(i):
+#         #         if nums[i]>nums[j]:
+#         #             dp[i] = max(dp[i], dp[j]+1)
+#         # return max(dp)
