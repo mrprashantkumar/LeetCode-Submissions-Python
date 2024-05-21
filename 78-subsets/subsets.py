44e2a4e246aca538d97b7f1f543d6e5596360144
Prashant Kumar <72797072+mrprashantkumar@users.noreply.github.com>
2024-05-21 08:10:30 +0530

    Time: 35 ms (72.93%) | Memory: 16.7 MB (77.55%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,11 +1,15 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        if not nums:
-            return [[]]
-        
-        ele = [nums[0]]
-        res = self.subsets(nums[1:])
+        def helper(i, subset):
+            if i == n:
+                self.ans.append(subset)
+                return 
 
-        ans = [arr[:] for arr in res]
-        ans += [arr[:]+ele for arr in res]
-        return ans
+            helper(i+1, subset)
+            helper(i+1, subset+[nums[i]])
+            return
+        
+        n = len(nums)
+        self.ans = []
+        helper(0, [])
+        return self.ans
